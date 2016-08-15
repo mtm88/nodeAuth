d3a908fff1e655c14cf8ceddcb6a9a91682ffba3
Michal <michal@hebeworks.com>
2016-08-15 11:51:24 +0100

    ticket arena fully working

--- eventsources/ticketarena.js
+++ eventsources/ticketarena.js
@@ -29,26 +29,31 @@ exports.process = function(req, res) {
 
 function getResults(lastPage) {
     const contentArray = [];
+    let totalEventsLength = 0;
     const promise = new Promise((resolve) => {
-        const url = 'http://www.ticketarena.co.uk/places/leeds/index.html?page=1';
         
-        request(url, (error, response, html) => {
-            if(!error) {
-            const $ = cheerio.load(html);
+        for (let i = 1; i <= lastPage; i++) {
+            console.log(`processing page ${i}`);
+            const url = `http://www.ticketarena.co.uk/places/leeds/index.html?page=${i}`;
+            
+            request(url, (error, response, html) => {
+                if(!error) {
+                const $ = cheerio.load(html);
 
-            const events = $('.eventresults').children('.itemwrap');
-            const eventsLength = events.length;
-            events.each((index, element) => {
+                const events = $('.eventresults').children('.itemwrap');
+                totalEventsLength += events.length;
+                console.log(`events to process: ${events.length}`);
+                events.each((index, element) => {
 
-                const title = $(element).children('.event-block').children('.event-text-container')
-                .find('h3').text();
-                const eventLink = $(element).children('.event-block').children('.event-text-container')
-                .find('a').attr('href');
-                const tempLocation = $(element).children('.event-block').children('.event-text-container')
-                .children('p').children().text().trim();
-                const location = tempLocation.slice(0, tempLocation.indexOf(','));
+                    const title = $(element).children('.event-block').children('.event-text-container')
+                    .find('h3').text();
+                    const eventLink = $(element).children('.event-block').children('.event-text-container')
+                    .find('a').attr('href');
+                    const tempLocation = $(element).children('.event-block').children('.event-text-container')
+                    .children('p').children().text().trim();
+                    const location = tempLocation.slice(0, tempLocation.indexOf(','));
 
-                const detailsUrl = `${eventLink}`;
+                    const detailsUrl = `${eventLink}`;
                     request(detailsUrl, (error, response, html) => {
                         if(!error) {
                         const $ = cheerio.load(html);
@@ -67,15 +72,15 @@ function getResults(lastPage) {
                                 startDate,
                                 endDate,
                             });
-                            if (contentArray.length === eventsLength) {
+                            if (contentArray.length === totalEventsLength) {
                                 resolve(contentArray);
                             }
                         }
                     });
+                });
+                }
             });
-            }
-        
-        });
+        }
     });
     return promise;
 }
